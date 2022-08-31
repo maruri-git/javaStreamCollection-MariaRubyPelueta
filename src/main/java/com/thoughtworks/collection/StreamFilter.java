de2a0540f59826a2ec123fa8f5e445d900b23dad
maruri-git <peluema.github@gmail.com>
2022-08-31 13:50:18 +0800

    feat: finish filter streams function

--- src/main/java/com/thoughtworks/collection/StreamFilter.java
+++ src/main/java/com/thoughtworks/collection/StreamFilter.java
@@ -6,15 +6,15 @@ import java.util.stream.Collectors;
 public class StreamFilter {
 
     public List<Integer> filterEven(List<Integer> numbers) {
-        return null;
+        return numbers.stream().filter(number -> number % 2 == 0).collect(Collectors.toList());
     }
 
     public List<String> filterEndWithS(List<String> words) {
-        return null;
+        return words.stream().filter(word -> word.endsWith("s")).collect(Collectors.toList());
     }
 
     public List<Integer> getCommonElements(List<Integer> numbers, List<Integer> anotherNumbers) {
-        return null;
+        return numbers.stream().filter(number -> anotherNumbers.contains(number)).collect(Collectors.toList());
     }
 
 
