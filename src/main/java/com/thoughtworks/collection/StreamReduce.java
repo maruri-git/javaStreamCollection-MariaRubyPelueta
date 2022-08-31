97cd2a03aa67927f39a87b52ac0245fccd9f831d
maruri-git <peluema.github@gmail.com>
2022-08-31 19:32:07 +0800

    feat: finish reduce streams function

--- src/main/java/com/thoughtworks/collection/StreamReduce.java
+++ src/main/java/com/thoughtworks/collection/StreamReduce.java
@@ -1,18 +1,22 @@
 package com.thoughtworks.collection;
 
 import java.util.List;
-
 public class StreamReduce {
 
     public int getLastOdd(List<Integer> numbers) {
-        return 0;
-    }
+        return numbers.stream()
+                .filter(number -> number % 1 == 0)
+                .reduce(0, (accumulator, number) -> number);
 
+    }
     public String getLongest(List<String> words) {
-        return null;
+       return words.stream()
+        .reduce("", (accumulator, word) -> word.length() > accumulator.length() ? word : accumulator);
     }
 
     public int getTotalLength(List<String> words) {
-        return 0;
+        return words.stream()
+                .map(String::length)
+                .reduce(0, (accumulator, word) -> accumulator + word);
     }
 }
